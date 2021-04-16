b82adff0ee9b16664187afc92bd194636ac23900
YuryVA <ghost_5990@mail.ru>
2021-04-16 20:34:31 +0300

    fix test_task1

--- tests/homework2/test_task1.py
+++ tests/homework2/test_task1.py
@@ -2,20 +2,20 @@ from homework2.task1 import get_longest_diverse_words, get_rarest_char, count_pu
 
 
 def test_get_longest_diverse_words():
-    assert get_longest_diverse_words('data.txt') == ['Bev\\u00f6lkerungsabschub', 'Machtbewu\\u00dftsein', 'Entz\\u00fcndbarkeit', 'r\\u00e9sistance-Bewegungen', 'Zahlenverh\\u00e4ltnis', 'sch\\u00e4ftsinhabers', 'ber\\u00fccksichtigen', 'Zahlenverh\\u00e4ltnisse', 'Termitenh\\u00fcgel', '\\u00dcberv\\u00f6lkerung']
+    assert get_longest_diverse_words('homework2\data.txt') == ['Bev\\u00f6lkerungsabschub', 'Machtbewu\\u00dftsein', 'Entz\\u00fcndbarkeit', 'r\\u00e9sistance-Bewegungen', 'Zahlenverh\\u00e4ltnis', 'sch\\u00e4ftsinhabers', 'ber\\u00fccksichtigen', 'Zahlenverh\\u00e4ltnisse', 'Termitenh\\u00fcgel', '\\u00dcberv\\u00f6lkerung']
 
 
 def test_get_rarest_char():
-    assert get_rarest_char('data.txt') == 'Y, X, (, )'
+    assert get_rarest_char('homework2\data.txt') == 'Y, X, (, )'
 
 
 def test_count_punctuation_chars():
-    assert count_punctuation_chars('data.txt') == 5255
+    assert count_punctuation_chars('homework2\data.txt') == 5255
 
 
 def test_count_non_ascii_chars():
-    assert count_non_ascii_chars('data.txt') == 2843
+    assert count_non_ascii_chars('homework2\data.txt') == 2843
 
 
 def test_get_most_common_non_ascii_char():
-    assert get_most_common_non_ascii_char('data.txt') == '\u00e4'
+    assert get_most_common_non_ascii_char('homework2\data.txt') == '\\u00e4'
