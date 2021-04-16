75c385728751b2448e83fd48f74f64eb2fa20f25
YuryVA <ghost_5990@mail.ru>
2021-04-16 20:37:30 +0300

    add task2

---
+++ tests/homework2/test_task2.py
@@ -0,0 +1,6 @@
+from homework2.task2 import major_and_minor_elem
+
+
+def test_major_and_minor_elem():
+    assert major_and_minor_elem([3, 2, 3]) == (3, 2)
+    assert major_and_minor_elem([2, 2, 1, 1, 1, 2, 2]) == (2, 1)
