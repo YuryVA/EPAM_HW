d760e9e4005f1e5eccb4740b99e9448b9648f01e
YuryVA <ghost_5990@mail.ru>
2021-04-18 19:24:18 +0300

    split tests for homework2 task1

---
+++ tests/homework2/test_task1-1.py
@@ -0,0 +1,16 @@
+from homework2.task1 import get_longest_diverse_words
+
+
+def test_get_longest_diverse_words():
+    assert get_longest_diverse_words("homework2\data.txt") == [
+        "Bev\\u00f6lkerungsabschub",
+        "Machtbewu\\u00dftsein",
+        "Entz\\u00fcndbarkeit",
+        "r\\u00e9sistance-Bewegungen",
+        "Zahlenverh\\u00e4ltnis",
+        "sch\\u00e4ftsinhabers",
+        "ber\\u00fccksichtigen",
+        "Zahlenverh\\u00e4ltnisse",
+        "Termitenh\\u00fcgel",
+        "\\u00dcberv\\u00f6lkerung",
+    ]
