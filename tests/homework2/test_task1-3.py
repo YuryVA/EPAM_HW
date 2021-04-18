68f366e0253a6914175dab830ab69ede12b83798
YuryVA <ghost_5990@mail.ru>
2021-04-18 20:12:59 +0300

    fixed 3 tests for homework2 task1

--- tests/homework2/test_task1-3.py
+++ tests/homework2/test_task1-3.py
@@ -2,4 +2,4 @@ from homework2.task1 import count_punctuation_chars
 
 
 def test_count_punctuation_chars():
-    assert count_punctuation_chars("homework2\\data.txt") == 5255
+    assert count_punctuation_chars("data.txt") == 5255
--- tests/homework2/test_task1-4.py
+++ tests/homework2/test_task1-4.py
@@ -2,4 +2,4 @@ from homework2.task1 import count_non_ascii_chars
 
 
 def test_count_non_ascii_chars():
-    assert count_non_ascii_chars("homework2\\data.txt") == 2843
+    assert count_non_ascii_chars("data.txt") == 2843
