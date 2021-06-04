50c0206c3ad3bdf58125ac2815f7bbd235aadb2d
YuryVA <ghost_5990@mail.ru>
2021-06-04 17:53:54 +0300

    rewrite with itertools homework1 task4

--- homework1/task4.py
+++ homework1/task4.py
@@ -4,17 +4,13 @@ Given four lists A, B, C, D of integer values,
     compute how many tuples (i, j, k, l) there are such that A[i] + B[j] + C[k] + D[l] is zero.
 We guarantee, that all A, B, C, D have same length of N where 0 ≤ N ≤ 1000.
 """
+from itertools import product
 from typing import List
 
 
 def check_sum_of_four(a: List[int], b: List[int], c: List[int], d: List[int]) -> int:
     """Compute how many tuples (i, j, k, l) there are such that a[i] + b[j] + c[k] + d[l] is zero"""
 
-    tup_count = 0
-    for a_elem in a:
-        for b_elem in b:
-            for c_elem in c:
-                for d_elem in d:
-                    if a_elem + b_elem + c_elem + d_elem == 0:
-                        tup_count += 1
+    list_tup_comb = product(a, b, c, d)
+    tup_count = sum([1 for tup_comb in list_tup_comb if sum(tup_comb) == 0])
     return tup_count
